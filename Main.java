7f38e86a0df0f0f3435dcc6f0df206a7f493a54c
Chris Murphy <cdmurphy@seas.upenn.edu>
2024-05-22 08:57:15 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Main class for the sentiment analysis program.
+ * 
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		/* Implement this in Part 4 */
+		
+	}
+
+}
